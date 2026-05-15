86087d796113c1c47186e7048e20a08116a45aa0
Fani-malba <fanimalba19@gmail.com>
2026-05-14 23:41:55 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+void main ()
+{
+  int a=0, b=0, res=0, sum=0, mul=0;
+
+    printf("Ingrese un numero: ");
+    scanf("%d", &a);
+    
+    printf("Ingrese un numero: ");
+    scanf("%d", &b);
+   
+    
+    sum=a+b;
+    res=a-b;
+    mul=a*b;
+    
+    printf("La suma es %d\n", sum );
+    printf("La resta es %d\n", res);
+    printf("La multiplicacion es %d\n", mul);
+}
